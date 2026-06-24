bf663158ca5b9a8c408bbe1ca3a52f653e6ac64b
Jeremie <jeremieandria00@gmail.com>
2026-06-24 10:55:47 +0200

    CSS partie Jeremie

--- pages/stats.php
+++ pages/stats.php
@@ -1,32 +1,39 @@
 <?php
-    include('../inc/functions.php');
-    $stats = get_jobs_stats();
+include('../inc/functions.php');
+$stats = get_jobs_stats();
 ?>
 <html>
-    <head>
-        <title>Statistiques par emploi</title>
-    </head>
-    <body>
-    <p><a href="index.php">&larr; Retour aux départements</a></p>
-    <h1>Statistiques par emploi</h1>
 
-    <table border="1">
-        <tr>
-            <th>Emploi</th>
-            <th>Hommes</th>
-            <th>Femmes</th>
-            <th>Total</th>
-            <th>Salaire moyen</th>
-        </tr>
-        <?php foreach ($stats as $row) { ?>
+<head>
+    <title>Statistiques par emploi</title>
+    <link rel="stylesheet" href="../design/theme-corporate/style.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <p><a href="index.php" class="btn">&larr; Retour aux départements</a></p>
+        <h1 class="mt">Statistiques par emploi</h1>
+
+        <table class="table" border="1">
             <tr>
-                <td><?= $row['title'] ?></td>
-                <td><?= $row['nb_hommes'] ?></td>
-                <td><?= $row['nb_femmes'] ?></td>
-                <td><?= $row['nb_total'] ?></td>
-                <td><?= number_format($row['salaire_moyen'], 0, ',', ' ') ?> €</td>
+                <th>Emploi</th>
+                <th>Hommes</th>
+                <th>Femmes</th>
+                <th>Total</th>
+                <th>Salaire moyen</th>
             </tr>
-        <?php } ?>
-    </table>
-    </body>
+            <?php foreach ($stats as $row) { ?>
+                <tr>
+                    <td><?= $row['title'] ?></td>
+                    <td><?= $row['nb_hommes'] ?></td>
+                    <td><?= $row['nb_femmes'] ?></td>
+                    <td><?= $row['nb_total'] ?></td>
+                    <td><?= number_format($row['salaire_moyen'], 0, ',', ' ') ?> €</td>
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
+</body>
+
 </html>
